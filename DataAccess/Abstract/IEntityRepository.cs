957b258f6189c64a37eae254ec46fc3e55450766
Yasemin ORAN <yasemin.oran@mostem.onmicrosoft.com>
2021-02-04 01:05:47 +0300

    Added

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //generic constraint
+    //class:referans tip
+    //ientity:ientity olabilir ya da ientity'yi implemente eden bir nesne olabilir
+    //new(): new'lenebilir olmalı
+   public interface IEntityRepository <T> where T:class,IEntity
+    {
+        List<T> GetAll(Expression<Func<T,bool>>filter=null);
+        T Get();
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+       
+    }
+}
--- Entities/Concrete/Product.cs
+++ Entities/Concrete/Product.cs
@@ -5,8 +5,10 @@ using System.Text;
 
 namespace Entities.Concrete
 {
-   public class Product:IEntity
+    public class Product : IEntity
+
     {
+     
 
         public int ProductId { get; set; }
         public int CategoryId { get; set; }
