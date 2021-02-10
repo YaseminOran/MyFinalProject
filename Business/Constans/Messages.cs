06c053f97bf9effc08cda96a78dde29412975dc6
Yasemin ORAN <yasemin.oran@mostem.onmicrosoft.com>
2021-02-11 00:46:25 +0300

    APII

---
+++ Business/Constans/Messages.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constans
+{
+   public static class Messages
+    {
+        public static string ProductAdded = "Ürün eklendi";
+        public static string ProductNameInvalid = "Ürün adı geçersiz";
+
+
+        internal static string MaintenanceTime = "sistem bakımda ";
+        internal static string ProductsListed = "Ürünler listelendi";
+    }
+}
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,7 +9,14 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-             ProductTest();
+            //Data Transformation Object
+            ProductTest();
+            //IoC 
+            //CategoryTest();
+        }
+
+        private static void CategoryTest()
+        {
             CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
             foreach (var category in categoryManager.GetAll())
             {
@@ -21,9 +28,18 @@ namespace ConsoleUI
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetProductDetails())
+            var result = productManager.GetProductDetails();
+
+            if (result.Success == true)
             {
-                Console.WriteLine(product.ProductName+"/"+product.CategoryName);
+                foreach (var product in result.Data)
+                {
+                    Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+                }
+            }
+            else
+            {
+                Console.WriteLine(result.Message);
             }
         }
     }
